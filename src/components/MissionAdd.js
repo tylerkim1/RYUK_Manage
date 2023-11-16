5a6fbc0df691d2e51d64369be8abb6314496eaf6
minwoo song <alsdnthd1234@gmail.com>
2023-11-16 23:26:11 +0900

    mission add

--- src/components/MissionAdd.js
+++ src/components/MissionAdd.js
@@ -2,23 +2,36 @@ import React, { useState } from 'react';
 
 import '../css/MissionAdd.css';
 
+import { networkrequest } from './Header/XHR';
+
 // 사이드바 컴포넌트
 function MissionAdd() {
   const [newMission, setNewMission] = useState({
     title:'',
-    type:'', //매일하력 시도하력
+    missionType:'', //매일하력 시도하력
   });
 
   return (
-    <div className='debug' id='sidebar-main'>
-      <div className='debug' >
-        <input value={newMission.title} onChange={(e) => {newMission.title=(e.target.value);}} />
-      </div>
-      <div className='debug' >
-        <input value={newMission.type} onChange={(e) => {newMission.type=(e.target.value);}} />
-      </div>
-      <div className='debug' >
-        <button type="button" onclick="alert('Hello World!')"> 미션 추가 </button>
+    <div className='debug' id='msadd-main'>
+      <div id='msadd-body'>
+        <div className='debug' >
+          미션 제목: 
+          <input value={newMission.title} onChange={(e) => {setNewMission(prev=>({...prev, title:e.target.value}))}} />
+        </div>
+        <div className='debug' >
+          미션 타입: 
+          <input value={newMission.type} onChange={(e) => {setNewMission(prev=>({...prev, missionType:e.target.value}))}} />
+        </div>
+        <div className='debug' id='msadd-submit'>
+          <div id='msadd-button'>
+            <button type="button" onClick={()=>{
+              console.log(newMission);
+              networkrequest('mission/add/', newMission, (data)=>{
+                alert(data.status);
+              });
+            }}> 미션 추가 </button>
+          </div>
+        </div>
       </div>
     </div>
     );
