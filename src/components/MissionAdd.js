bf1d155298de9fe35ee401b2430e7e295330d022
minwoo song <alsdnthd1234@gmail.com>
2023-11-16 22:50:57 +0900

    ttt

--- src/components/MissionAdd.js
+++ src/components/MissionAdd.js
@@ -1,10 +1,25 @@
 import React, { useState } from 'react';
 
+import '../css/MissionAdd.css';
+
 // 사이드바 컴포넌트
 function Sidebar() {
+  const [newMission, setNewMission] = useState({
+    title:'',
+    type:'', //매일하력 시도하력
+  });
+
   return (
-    <div>
-        hello
+    <div className='debug' id='sidebar-main'>
+      <div className='debug' >
+        <input value={newMission.title} onChange={(e) => {newMission.title=(e.target.value);}} />
+      </div>
+      <div className='debug' >
+        <input value={newMission.type} onChange={(e) => {newMission.type=(e.target.value);}} />
+      </div>
+      <div className='debug' >
+        <button type="button" onclick="alert('Hello World!')"> 미션 추가 </button>
+      </div>
     </div>
     );
 }
