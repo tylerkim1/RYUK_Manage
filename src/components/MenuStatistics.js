ef749ad1ca990b58970c9b3b3ee3ad50997571b4
minwoo song <alsdnthd1234@gmail.com>
2023-11-23 22:36:06 +0900

    statmenu scroll

--- src/components/MenuStatistics.js
+++ src/components/MenuStatistics.js
@@ -12,16 +12,186 @@ function MenuStatistics() {
   const [unitsubject, setUnitSubjecct] = useState("individual");
   const [graphdata, setGraphData] = useState({data:{
     usrid_1231:{
+      name:"honggild",
       mission_date:"202013201",
       num_mission:4,
       num_success:2,
       percentage:50
     },
     usrid_8501:{
+      name:"emu",
       mission_date:"202013201",
       num_mission:100,
       num_success:42,
       percentage:42
+    },
+    usrid_8277:{
+      name:"cat",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:29,
+      percentage:97
+    },
+    usrid_8278:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_8291:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_8290:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_8295:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_8299:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_8197:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_8177:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_8297:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_8287:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_7278:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_7291:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_7290:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_7295:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_7299:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_7197:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_7177:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_7297:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_7287:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_6299:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_6197:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_6177:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_6297:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
+    },
+    usrid_6287:{
+      name:"nono",
+      mission_date:"202013201",
+      num_mission:30,
+      num_success:1,
+      percentage:3
     }
   }});
 
@@ -75,9 +245,9 @@ function MenuStatistics() {
         </div>
       </div>
       <div id="ms-lower">
-          {
-            Object.entries(graphdata.data).map(([usr_id,info]) => GraphBar(usr_id, info.num_success, info.num_mission, info.percentage))
-          }
+        {
+          Object.entries(graphdata.data).map(([usr_id,info]) => GraphBar(info.name, info.num_success, info.num_mission, info.percentage))
+        }
       </div>
     </div>
   );
