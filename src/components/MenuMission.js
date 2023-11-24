abdbdee3c56b414826c2421bca935ee0021a7483
tyler <tylerkim129@gmail.com>
2023-11-25 04:00:06 +0900

    temp

--- src/components/MenuMission.js
+++ src/components/MenuMission.js
@@ -18,39 +18,18 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Avatar from '@mui/material/Avatar';
-import Chip from '@mui/material/Chip';
-import IconButton from '@mui/material/IconButton';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 // 스타일을 적용한 컴포넌트들을 정의합니다.
 const StyledTableContainer = styled(TableContainer)(({ theme }) => ({
-  marginTop: theme.spacing(3),
+  // marginTop: theme.spacing(3),
+  width: "100%",
 }));
 
-const StyledAvatar = styled(Avatar)(({ theme }) => ({
-  marginRight: theme.spacing(2),
-}));
-
-const StatusChip = styled(Chip)(({ status, theme }) => ({
-  ...(status === 'ontime' && {
-    backgroundColor: theme.palette.success.main,
-    color: theme.palette.common.white,
-  }),
-  ...(status === 'late' && {
-    backgroundColor: theme.palette.error.main,
-    color: theme.palette.common.white,
-  }),
-}));
-
-
-const MenuMission = (e) => {
-  const today = dayjs();
-  const { teams, missionPool } = useMission();
+const MenuMission = () => {
+  const { teams, missionPool, missions, getMissions } = useMission();
   const [selectedMission, setSelectedMission] = useState();
   const [selectedTeam, setSelectedTeam] = useState(null);
   const [selectedCategory, setSelectedCategory] = useState(0);
-  const [startDate, setStartDate] = useState(null);
   const [selectedDate, setSelectedDate] = useState(null);
   const categories = ['전체', '매일하력', '시도해력', '마음봄력', '유유자력', '레벨업력'];
 
@@ -59,28 +38,6 @@ const MenuMission = (e) => {
   const handleDateChange = (newValue) => {
     setSelectedDate(newValue);
   };
-
-  // 미션 데이터 배열
-  const missions1 = [
-    {
-      title: '미션 1',
-      achieveLevel: '3/3',
-      participants: [
-        { name: '쿵짝님', image: missionImage, desc: '오늘 과일을 썰어 먹었어요!' },
-        { name: '쿵쿵님', image: missionImage, desc: '오늘 과일을 썰어 먹었어요!' },
-        { name: '쿵야님', image: missionImage, desc: '오늘 과일을 썰어 먹었어요!' }
-      ]
-    },
-    {
-      title: '미션 2',
-      achieveLevel: '2/3',
-      participants: [
-        { name: '쿵짝님', image: missionImage, desc: '오늘 과일을 썰어 먹었어요!' },
-        { name: '쿵쿵님', image: missionImage, desc: '오늘 과일을 썰어 먹었어요!' },
-        { name: '쿵야님', image: null, desc: '아직 미션을 완료하지 못했어요.' }
-      ]
-    }
-  ];
 
   const handleAddMissionToTeam = (missionId, teamId) => {
     console.log(missionId, teamId)
@@ -136,78 +93,22 @@ const MenuMission = (e) => {
           />
         </LocalizationProvider>
       </div>
-      {/* <div id="menu-mission-body">
-        <div id="common-mission-list">
-          {missions1.map((mission, missionIndex) => (
-            <div key={missionIndex} id="common-mission">
-              <div id="common-mission-header">
-                <div id="common-mission-title">
-                  <span>{mission.title}</span>
-                </div>
-                <div id="common-mission-achieve-level">
-                  <span>달성률: {mission.achieveLevel}</span>
-                </div>
-              </div>
-              <div id="common-mission-body">
-                <div id="common-mission-personal-list">
-                  {mission.participants.map((participant, participantIndex) => (
-                    <div key={participantIndex} id="common-mission-personal" style={{ backgroundColor: participant.image ? '#ADFF0026' : '#FF000026' }}>
-                      <div id="common-mission-personal-name">
-                        <span>{participant.name}</span>
-                      </div>
-                      {participant.image && (
-                        <div id="common-mission-personal-image">
-                          <img src={participant.image} alt="user" />
-                        </div>
-                      )}
-                      <div id="common-mission-personal-desc" style={{ alignItems: participant.image ? 'flex-start' : 'center' }}>
-                        <span>{participant.desc}</span>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div> */}
-      (
       <StyledTableContainer component={Paper}>
         <Table aria-label="customized table">
           <TableHead>
             <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Client</TableCell>
-              <TableCell>Pickup Address</TableCell>
-              <TableCell>Dropoff Address</TableCell>
-              <TableCell>Courier</TableCell>
-              <TableCell>Status</TableCell>
+              <TableCell>미션 제목</TableCell>
+              <TableCell>카테고리</TableCell>
+              <TableCell>성취율</TableCell>
               <TableCell></TableCell>
             </TableRow>
           </TableHead>
           {/* <TableBody>
             {rows.map((row) => (
               <TableRow key={row.id}>
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell>
-                  <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <StyledAvatar />
-                    {row.client}
-                  </div>
-                </TableCell>
                 <TableCell>{row.pickupAddress}</TableCell>
                 <TableCell>{row.dropoffAddress}</TableCell>
                 <TableCell>{row.courier}</TableCell>
-                <TableCell>
-                  <StatusChip label={row.status} status={row.status} />
-                </TableCell>
-                <TableCell>
-                  <IconButton>
-                    <MoreVertIcon />
-                  </IconButton>
-                </TableCell>
               </TableRow>
             ))}
           </TableBody> */}
--- src/components/hooks/useMission.js
+++ src/components/hooks/useMission.js
@@ -1,16 +1,30 @@
 import { useState, useEffect } from 'react';
 import { networkrequest } from '../Header/XHR';
+import dayjs from 'dayjs';
 
 const useMission = () => {
   const [teams, setTeams] = useState([]);
   const [missionPool, setMissionPool] = useState([]);
+  const [missions, setMissions] = useState([]);
+
+  const today = dayjs().format("YYYY_MM_DD");
 
   useEffect(() => {
-    networkrequest('team/all/', {}, (data) => { setTeams(data.data); });
-    networkrequest('mission/all/', {}, (data) => { setMissionPool(data.data); });
+    networkrequest('team/all/', {}, (data) => setTeams(data.data));
+    networkrequest('mission/all/', {}, (data) => setMissionPool(data.data));
   }, []);
+  
+  useEffect(() => {
+    if (teams.length > 0) {
+      getMissions(today, teams[0].team_id);
+    }
+  }, [teams]); // teams 배열이 변경될 때마다 이 useEffect가 실행됩니다.
 
-  return { teams, missionPool };
+  const getMissions = (date, teamId) => {
+    networkrequest('mission/getTeam/', {date: date, teamId: teamId}, (data) => setMissions(data.data))
+  }
+
+  return { teams, missionPool, missions, getMissions };
 };
 
 export default useMission;
