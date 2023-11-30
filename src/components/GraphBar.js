97ef5786cdba31367d693fdee7ed921b55e2d584
tylerkim <tylerkim129@gmail.com>
2023-11-30 22:18:45 +0900

    statistic UI change

--- src/components/GraphBar.js
+++ src/components/GraphBar.js
@@ -1,7 +1,9 @@
 import '../css/MenuStatistics.css';
 
-export const GraphBar = (name, success, total, percent) => <div className='graphbar-bg'>
+export const GraphBar = (name, success, total, percent) => (
+<div className='graphbar-bg'>
     <div className='graphbar-item' style={{width: percent + '%'}}>
         {name + ': ' + percent + "% (" + success + " / " + total + ")"}
     </div>
 </div>
+)
--- src/components/MenuStatistics.js
+++ src/components/MenuStatistics.js
@@ -3,56 +3,54 @@ import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { TextField } from '@mui/material';
 import '../css/MenuStatistics.css';
-
+import { FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import { GraphBar } from './GraphBar';
 import { networkrequest } from './Header/XHR';
+import dayjs from 'dayjs';
 
 function MenuStatistics() {
   const [unittime, setUnitTime] = useState("daily");
   const [unitsubject, setUnitSubjecct] = useState("individual");
 
-  const [targetdate, setTargetDate] = useState("2023_11_16");
+  const [targetdate, setTargetDate] = useState(dayjs());
   const [targetmonth, setTargetMonth] = useState("2023_11_30");
 
   const [graphdata, setGraphData] = useState({
-    status:'ok',
-    data:[
+    status: 'ok',
+    data: [
       {
-        nickname:'1',
-        num_mission:30,
-        num_success:10,
-        percentage:33
+        nickname: '1',
+        num_mission: 30,
+        num_success: 10,
+        percentage: 33
       }
     ],
-    isloaded:'no'
+    isloaded: 'no'
   });
 
   console.log(graphdata);
 
   const loadGraphData = () => {
-    if(unitsubject == "individual")
-    {
+    if (unitsubject == "individual") {
       // alert("day indiv " + targetdate);
-      networkrequest('stats/allDaily', {date:targetdate}, setGraphData);
+      networkrequest('stats/allDaily', { date: targetdate }, setGraphData);
     }
-    else{
+    else {
       // alert("day team " + targetdate);
-      networkrequest('stats/allTeamSumDaily', {date:targetdate}, setGraphData);
+      networkrequest('stats/allTeamSumDaily', { date: targetdate }, setGraphData);
     }
   }
 
   const loadGraphDataMonth = () => {
     const netmonth = targetmonth.substring(0, targetmonth.length - 2) + '31';
-    if(unitsubject == "individual")
-    {
+    if (unitsubject == "individual") {
       // alert("month indiv " + netmonth);
-      networkrequest('stats/allMonth', {date:netmonth}, setGraphData);
+      networkrequest('stats/allMonth', { date: netmonth }, setGraphData);
     }
-    else{
+    else {
       // alert("month team " + netmonth);
-      networkrequest('stats/allTeamSumMonth', {date:netmonth}, setGraphData);
+      networkrequest('stats/allTeamSumMonth', { date: netmonth }, setGraphData);
     }
   }
 
@@ -67,14 +65,11 @@ function MenuStatistics() {
     setTargetMonth(new_targ.format("YYYY_MM_DD"));
   }
 
-  if(graphdata.isloaded == 'no')
-  {
-    if(unittime == "monthly")
-    {
+  if (graphdata.isloaded == 'no') {
+    if (unittime == "monthly") {
       loadGraphDataMonth();
     }
-    else
-    {
+    else {
       loadGraphData();
     }
   }
@@ -82,54 +77,91 @@ function MenuStatistics() {
   return (
     <div id="menu-stat-root">
       <div id="ms-upper">
-        <div id="ms-datebar">
-          {
-            (unittime == "daily") ?
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DemoContainer components={['DesktopDateTimePicker']}>
-                <DemoItem>
-                  <DesktopDatePicker
-                    label="--"
-                    value={targetdate}
-                    onChange={targetdateChange}
-                    renderInput={(params) => <TextField {...params} error={false} />} />
-                  {/* <DesktopDatePicker onChange={loadGraphData} /> */}
-                </DemoItem>
-              </DemoContainer>
-            </LocalizationProvider>
-            :
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DemoContainer components={['DesktopDateTimePicker']}>
-                <DemoItem>
-                  <DesktopDatePicker views={['month', 'year']}
-                    value={targetdate}
-                    onChange={targetMonthChange} 
-                    renderInput={(params) => <TextField {...params} error={false} />} />
-                </DemoItem>
-              </DemoContainer>
-            </LocalizationProvider>
-          }
+        <div id="menu-mission-header-selector">
+          <FormControl className="mui-select" style={{ width: '100px' }}>
+            <InputLabel className="mui-select-label" id="mui-select-team-label">팀</InputLabel>
+            <Select
+              labelId="mui-select-team-label"
+              value={unittime}
+              label="기간"
+              onChange={(e) => { graphdata['isloaded'] = 'no'; setUnitTime(e.target.value); }}
+            >
+              <MenuItem key="daily" value={"daily"}>일별</MenuItem>
+              <MenuItem key="monthly" value={"monthly"}>월별</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className="mui-select" style={{ width: '100px' }}>
+            <InputLabel className="mui-select-label" id="mui-select-team-label">팀</InputLabel>
+            <Select
+              labelId="mui-select-team-label"
+              value={unitsubject}
+              label="기간"
+              onChange={(e) => { graphdata['isloaded'] = 'no'; setUnitSubjecct(e.target.value); }}
+            >
+              <MenuItem key="individual" value={"individual"}>개인별</MenuItem>
+              <MenuItem key="team" value={"team"}>팀별</MenuItem>
+            </Select>
+          </FormControl>
         </div>
-        <div id="ms-optionbar">
-          <span style={{paddingLeft: '10px', paddingRight: '10px'}}>
-            기간 :
-          </span>
-          <select value={unittime} onChange={(e) => {graphdata['isloaded'] = 'no'; setUnitTime(e.target.value);}}>
-            <option key="daily" value={"daily"}>일별</option>
-            <option key="monthly" value={"monthly"}>월별</option>
-          </select>
-          <span style={{padding: '10px', paddingRight: '10px'}}>
-            인원 :
-          </span>
-          <select value={unitsubject} onChange={(e) => {graphdata['isloaded'] = 'no'; setUnitSubjecct(e.target.value);}}>
-            <option key="individual" value={"individual"}>개인별</option>
-            <option key="team" value={"team"}>팀별</option>
-          </select>
-        </div>
+        {/* <div id="ms-datebar"> */}
+        {
+          (unittime == "daily") ?
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DesktopDatePicker
+                label="날짜"
+                value={targetdate}
+                onChange={targetdateChange}
+                renderInput={(params) => <TextField {...params} />}
+              />
+              {/* <DesktopDatePicker onChange={loadGraphData} /> */}
+            </LocalizationProvider> :
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DesktopDatePicker
+                views={['month', 'year']}
+                label="날짜"
+                value={targetdate}
+                onChange={targetMonthChange}
+                renderInput={(params) => <TextField {...params} />}
+              />
+              {/* <DesktopDatePicker onChange={loadGraphData} /> */}
+            </LocalizationProvider>
+          // <LocalizationProvider dateAdapter={AdapterDayjs}>
+          //   <DemoContainer components={['DesktopDateTimePicker']}>
+          //     <DemoItem>
+          //       <DesktopDatePicker
+          //         label="날짜"
+          //         value={targetdate}
+          //         onChange={targetdateChange}
+          //         renderInput={(params) => <TextField {...params} error={false} />} />
+          //       {/* <DesktopDatePicker onChange={loadGraphData} /> */}
+          //     </DemoItem>
+          //   </DemoContainer>
+          // </LocalizationProvider>
+          // :
+          // <LocalizationProvider dateAdapter={AdapterDayjs}>
+          //   <DemoContainer components={['DesktopDateTimePicker']}>
+          //     <DemoItem>
+          //       <DesktopDatePicker views={['month', 'year']}
+          //         label="날짜"
+          //         value={targetdate}
+          //         onChange={targetMonthChange}
+          //         renderInput={(params) => <TextField {...params} error={false} />} />
+          //     </DemoItem>
+          //   </DemoContainer>
+          // </LocalizationProvider>
+        }
+        {/* </div> */}
       </div>
       <div id="ms-lower">
-        {
-          graphdata.data.filter(it => it.num_mission != 0).map((it) => GraphBar(it.nickname, it.num_success, it.num_mission, it.percentage))
+        {/* {console.log(graphdata.data)}
+        { graphdata.data ? graphdata.data.filter(it => it.num_mission != 0).map((it) => GraphBar(it.nickname, it.num_success, it.num_mission, it.percentage)) : "결과가 없습니다."
+        } */}
+        {graphdata.data && graphdata.data.some(it => it.num_mission !== 0) ?
+          graphdata.data
+          .filter(it => it.num_mission !== 0)
+          .sort((a, b) => b.percentage - a.percentage) // 내림차순 정렬
+          .map((it) => GraphBar(it.nickname, it.num_success, it.num_mission, it.percentage))
+        : "No data" 
         }
       </div>
     </div>
